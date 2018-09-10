0b4d41194f8740f86cf41dfd81f1b1f2d53b1cf4
Claw <blue01claws@gmail.com>
2018-09-10 17:57:59 +0530

    Logical errors.

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
 generate: salary_calc.c
 	gcc -Wall salary_calc.c -o SalaryCalculator
-	mkdir build
 	mv SalaryCalculator build/
--- salary_calc.c
+++ salary_calc.c
@@ -4,7 +4,7 @@
 	House Rent = 20% of x
 	Gross salary = ?
 	Calc:
-		Gross Salary = (x - (0.4*x + 0.2*x))
+		Gross Salary = (x + (0.4*x + 0.2*x))
 */
 #include<stdio.h>
 int main(void)
@@ -17,7 +17,7 @@ int main(void)
 
 	Dear_all = 0.4 * Basic_sal;
 	House_re = 0.2 * Basic_sal;
-	Gross_sal = Basic_sal - (Dear_all + House_re);
+	Gross_sal = Basic_sal + (Dear_all + House_re);
 	Gross_sal_int = (int) Gross_sal;
 
 	printf("Gross salary is: %d\n", Gross_sal_int);
